927502d23802b768c3017eb716745e80a4491ecc
kaiserawu <kaiser.a.wu@gmail.com>
2019-03-12 21:01:36 -0700

    Import dummy data into App file

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import DummyData from '../../dummy-data'
+
 import './App.css';
+
 
 class App extends Component {
   render() {
